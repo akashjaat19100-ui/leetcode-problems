bc71c732aeee2356be30d485b82f0d2f0281cf3a
Akash dhillon <akashjaat19100@gmail.com>
2026-09-16 21:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (83.69%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long original =n;
-        long long ans = n;
-        long long count=0;
-        long long x=0;
-        while(n>0){
-            n=n/10;
-            count++;
-            
+        long long ans = 0;
+        long long start = 1000;
+        long long commas = 1;
+
+        while (start <= n) {
+            long long end = start * 1000 - 1;
+
+            if (end > n)
+                end = n;
+
+            ans += (end - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
         }
-        if(count<4){
-            x=0;
-        } 
-        else if(count>=4 && count <7){
-            x=original-1000;
-            x=x+1;
-        }
-        else if(count>=7&& count<10){
-            x= original-1000000;
-            x=(x*2);
-            x=x+999000;
-            x=x+2;
-        }
-        else if(count>=10&& count<13){
-            x = original-1000000000;
-            x=(x*3);
-            x= x+1998999000;
-            x=x+3;
-           
-        }
-        else if(count>=13 && count<16){
-           x = original-1000000000000;
-            x=(x*4);
-            x= x+2998998999000;
-            x=x+4;
-        }
-        else{
-            x = original - 1000000000000000;
-        x=(x*5);
-            x= x+3998998998999000LL;
-            x=x+5;
-           
-        }
-       return x;
+
+        return ans;
     }
-        
 };
-   
